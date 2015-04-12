4f89a5cdebf92cc2e82d32d65a2c323ad9997b75
Xing Li <shangwangwanwan@gmail.com>
2015-04-12 14:23:33 +0800

    Found need knowledge of CFNetwork
    Will learn about this framework before continue

--- RadioExample/RadioExample/ViewController.m
+++ RadioExample/RadioExample/ViewController.m
@@ -9,6 +9,25 @@
 #import "ViewController.h"
 #import <AudioToolbox/AudioToolbox.h>
 
+typedef struct MyStreamStruct{
+    
+}MyStreamStruct;
+
+void MyPropertyListenerCallback( void *inClientData,
+                                AudioFileStreamID inAudioFileStream,
+                                AudioFileStreamPropertyID inPropertyID,
+                                UInt32 *ioFlags ){
+    
+}
+
+void MyPacketCallback( void *inClientData,
+                      UInt32 inNumberBytes,
+                      UInt32 inNumberPackets,
+                      const void *inInputData,
+                      AudioStreamPacketDescription *inPacketDescriptions ){
+    
+}
+
 @interface ViewController ()
 
 @end
@@ -19,6 +38,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSString* availableRadioURL = @"http://m.hz.qingting.fm/live/4968.m3u8";
+    
+    MyStreamStruct* myStream;
+    AudioFileStreamID audioFileStream;
+    
+    AudioFileStreamOpen(&myStream,
+                        MyPropertyListenerCallback,
+                        MyPacketCallback,
+                        0,
+                        &audioFileStream);
+    
+    //TODO:Need CFNetwork knowledge.
+    //Will get knowledge of this framework before continue.
+    
+    /*
+    AudioFileStreamParseBytes(audioFileStream,
+                              <#UInt32 inDataByteSize#>, <#const void *inData#>, <#UInt32 inFlags#>)
+     */
 }
 
 - (void)didReceiveMemoryWarning {
